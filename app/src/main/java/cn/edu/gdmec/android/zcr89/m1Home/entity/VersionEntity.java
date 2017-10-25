d14f428a155ebb88a66aa7e85341015dec86402a
13124996357 <2501867629@qq.com>
2017-10-25 18:26:01 +0800

    2

---
+++ app/src/main/java/cn/edu/gdmec/android/zcr89/m1Home/entity/VersionEntity.java
@@ -0,0 +1,9 @@
+package cn.edu.gdmec.android.zcr89.m1Home.entity;
+
+
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/zcr89/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,9 @@
+package cn.edu.gdmec.android.zcr89.m2theftguard.entity;
+
+
+
+public class ContactInfo {
+    public String phone;
+    public String id;
+    public String name;
+}
